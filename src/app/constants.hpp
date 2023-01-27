706b7e1c05f9dcea6bbf4ad23ddc4f1ab5e71d8f
Furyn <vincent_1999@hotmail.fr>
2023-01-27 09:43:15 +0100

    [FIX] Max disctance player

--- src/app/constants.hpp
+++ src/app/constants.hpp
@@ -8,7 +8,7 @@ constexpr std::uint32_t TickRate = 26; //< Pour matcher avec unity
 constexpr float gameTickInterval = 1.f / 30.f;
 constexpr float networkTickInterval = 1.f / 30.f;
 
-constexpr float playerBoundaryValue = 5.f;
+constexpr float playerBoundaryValue = 4.f;
 constexpr float playerSpeed = 10.f;
 
 constexpr float ballBoundaryValue = 5.f;
